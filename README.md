744244afc9cdd1f1bc1194aa7611ddda4377b3dc
Jānis Kiršteins <janis@janiskirsteins.org>
2021-12-27 09:55:49 +0100

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# ServerFramework
+
+A description of this package.
---
+++ Sources/ServerFramework/ServerFramework.swift
@@ -0,0 +1,6 @@
+public struct ServerFramework {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
